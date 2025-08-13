9f69a3b851923c53ac68790d6d2eaf7866a114d3
Skulled2010 <hle564270@gmail.com>
2025-08-13 08:51:40 +0700

    Update api.py

--- api.py
+++ api.py
@@ -3,12 +3,10 @@ import os
 
 app = Flask(__name__)
 
-# Định nghĩa một route API cơ bản
 @app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify({"message": "Xin chào! Đây là API của bạn."})
 
-# Chạy ứng dụng
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Lấy cổng từ Render, mặc định 5000 nếu chạy cục bộ
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
